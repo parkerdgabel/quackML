b59f1ae46bb6e13ceaeaff0f3e8f8eeab98c93f1
Parker Gabel <parker.d.gabel@gmail.com>
2024-08-20 11:32:29 -0700

    Added task module

---
+++ src/orm/task.rs
@@ -0,0 +1,172 @@
+use serde::Deserialize;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[allow(non_camel_case_types)]
+pub enum Task {
+    regression,
+    classification,
+    decomposition,
+    clustering,
+    question_answering,
+    summarization,
+    translation,
+    text_classification,
+    text_generation,
+    text2text,
+    embedding,
+    text_pair_classification,
+    conversation,
+}
+
+impl Task {
+    pub fn is_classification(&self) -> bool {
+        match self {
+            Task::classification
+            | Task::text_classification
+            | Task::text_pair_classification
+            | Task::conversation => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_regression(&self) -> bool {
+        match self {
+            Task::regression => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_text_classification(&self) -> bool {
+        match self {
+            Task::text_classification | Task::text_pair_classification => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_text_generation(&self) -> bool {
+        match self {
+            Task::text_generation | Task::text2text => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_embedding(&self) -> bool {
+        match self {
+            Task::embedding => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_conversation(&self) -> bool {
+        match self {
+            Task::conversation => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_supervised(&self) -> bool {
+        matches!(self, Task::regression | Task::classification)
+    }
+
+    pub fn default_target_metric(&self) -> String {
+        match self {
+            Task::regression => "r2",
+            Task::classification => "f1",
+            Task::decomposition => "cumulative_explained_variance",
+            Task::clustering => "silhouette",
+            Task::question_answering => "f1",
+            Task::translation => "blue",
+            Task::summarization => "rouge_ngram_f1",
+            Task::text_classification => "f1",
+            Task::text_generation => "perplexity",
+            Task::text2text => "perplexity",
+            Task::embedding => panic!("No default target metric for embedding task"),
+            Task::text_pair_classification => "f1",
+            Task::conversation => "bleu",
+        }
+        .to_string()
+    }
+
+    pub fn default_target_metric_positive(&self) -> bool {
+        match self {
+            Task::regression => true,
+            Task::classification => true,
+            Task::decomposition => true,
+            Task::clustering => true,
+            Task::question_answering => true,
+            Task::translation => true,
+            Task::summarization => true,
+            Task::text_classification => true,
+            Task::text_generation => false,
+            Task::text2text => false,
+            Task::embedding => panic!("No default target metric positive for embedding task"),
+            Task::text_pair_classification => true,
+            Task::conversation => true,
+        }
+    }
+
+    pub fn value_is_better(&self, value: f64, other: f64) -> bool {
+        if self.default_target_metric_positive() {
+            value > other
+        } else {
+            value < other
+        }
+    }
+
+    pub fn default_target_metric_sql_order(&self) -> String {
+        let direction = if self.default_target_metric_positive() {
+            "DESC"
+        } else {
+            "ASC"
+        };
+        format!(
+            "ORDER BY models.metrics->>'{}' {} NULLS LAST",
+            self.default_target_metric(),
+            direction
+        )
+    }
+}
+
+impl std::str::FromStr for Task {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Task, Self::Err> {
+        match input {
+            "regression" => Ok(Task::regression),
+            "classification" => Ok(Task::classification),
+            "decomposition" => Ok(Task::decomposition),
+            "clustering" => Ok(Task::clustering),
+            "question-answering" | "question_answering" => Ok(Task::question_answering),
+            "summarization" => Ok(Task::summarization),
+            "translation" => Ok(Task::translation),
+            "text-classification" | "text_classification" => Ok(Task::text_classification),
+            "text-generation" | "text_generation" => Ok(Task::text_generation),
+            "text2text" => Ok(Task::text2text),
+            "text-pair-classification" | "text_pair_classification" => {
+                Ok(Task::text_pair_classification)
+            }
+            "conversation" => Ok(Task::conversation),
+            _ => Err(()),
+        }
+    }
+}
+
+impl std::string::ToString for Task {
+    fn to_string(&self) -> String {
+        match *self {
+            Task::regression => "regression".to_string(),
+            Task::classification => "classification".to_string(),
+            Task::decomposition => "decomposition".to_string(),
+            Task::clustering => "clustering".to_string(),
+            Task::question_answering => "question-answering".to_string(),
+            Task::summarization => "summarization".to_string(),
+            Task::translation => "translation".to_string(),
+            Task::text_classification => "text-classification".to_string(),
+            Task::text_generation => "text-generation".to_string(),
+            Task::text2text => "text2text".to_string(),
+            Task::embedding => "embedding".to_string(),
+            Task::text_pair_classification => "text-pair-classification".to_string(),
+            Task::conversation => "conversation".to_string(),
+        }
+    }
+}
