fe9735d099c239c458ea066c7bb7603446dca901
Parker Gabel <parker.d.gabel@gmail.com>
2024-09-06 08:45:46 -0700

    Debug stuff

--- .nvim-dap.lua
+++ .nvim-dap.lua
@@ -18,10 +18,20 @@ dap.configurations.rust = {
 		program = function()
 			return vim.fn.getcwd() .. "/duckdb-rs/duckdb/build/debug/duckdb"
 		end,
-		args = { "-unsigned" },
+		args = { "-unsigned", "-cmd", "set allow_extensions_metadata_mismatch=true;load 'quack_ml.duckdb_extension'" },
 		stopOnEntry = false,
 		postRunCommands = {
 			"target modules add quack_ml.duckdb_extension",
 		},
 	},
 }
+
+dap.configurations.rust[1] = setmetatable(dap.configurations.rust[1], {
+	__call = function(config)
+		vim.fn.system(
+			'RUSTFLAGS="-C link-arg=-Wl,-rpath,/usr/lib -L /opt/homebrew/opt/libomp/lib -L /opt/homebrew/Cellar/python@3.11/3.11.9_1/Frameworks/Python.framework/Versions/3.11/lib" cargo build'
+		)
+		vim.fn.system("cp ./target/debug/libquack_ml.dylib quack_ml.duckdb_extension")
+		return config
+	end,
+})
